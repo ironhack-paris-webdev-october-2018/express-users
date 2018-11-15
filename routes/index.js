85114bb67552a3ffe8a2cb8bf3644a1fd767ddc7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-11-15 12:43:35 +0100

    Add user settings page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const User = require("../models/user-model.js");
+
 const router  = express.Router();
 
 /* GET home page */
@@ -14,5 +17,32 @@ router.get('/', (req, res, next) => {
 
   res.render('index');
 });
+
+router.get("/settings", (req, res, next) => {
+  // AUTHORIZATION: You have to be logged-in to visit this page
+  if (!req.user) {
+    req.flash("error", "You have to be logged-in to visit User Settings!");
+    res.redirect("/login");
+  }
+  else {
+    res.render("settings-page.hbs");
+  }
+});
+
+router.post("/process-settings", (req, res, next) => {
+  const { fullName, email } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id, // the logged-in user's ID from Passport's "req.user"
+    { $set: { fullName, email } },
+    { runValidators: true },
+  )
+  .then(userDoc => {
+    req.flash("success", "Settings saved! 😉");
+    res.redirect("/");
+  })
+  .catch(err => next(err));
+});
+
 
 module.exports = router;
