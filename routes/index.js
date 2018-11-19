69881c0707153b32d5dfe88463b6b08a8720655c
abisummers <hello@abisummers.com>
2018-11-19 15:56:19 +0100

    added file upload

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
-const express = require('express');
-
+const express = require("express");
 const User = require("../models/user-model.js");
-
-const router  = express.Router();
+const router = express.Router();
+const fileUploader = require("../config/file-upload.js");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // "req.user" comes from Passport's "deserializeUser()"
   // (it's the document of the currently logged in user)
   if (req.user) {
     console.log("LOGGED IN! 🐉", req.user);
-  }
-  else {
+  } else {
     console.log("NOT logged in 😢", req.user);
   }
 
-  res.render('index');
+  res.render("index");
 });
 
 router.get("/settings", (req, res, next) => {
@@ -23,26 +21,37 @@ router.get("/settings", (req, res, next) => {
   if (!req.user) {
     req.flash("error", "You have to be logged-in to visit User Settings!");
     res.redirect("/login");
-  }
-  else {
+  } else {
     res.render("settings-page.hbs");
   }
 });
 
-router.post("/process-settings", (req, res, next) => {
-  const { fullName, email } = req.body;
+router.post(
+  "/process-settings",
 
-  User.findByIdAndUpdate(
-    req.user._id, // the logged-in user's ID from Passport's "req.user"
-    { $set: { fullName, email } },
-    { runValidators: true },
-  )
-  .then(userDoc => {
-    req.flash("success", "Settings saved! 😉");
-    res.redirect("/");
-  })
-  .catch(err => next(err));
-});
+  // you need to use avatarUpload in the settings-page
+  fileUploader.single("avatarUpload"),
+  (req, res, next) => {
+    const { fullName, email } = req.body;
 
+    let toUpdate = { fullName, email };
+
+    //multer stores the file in req.file
+    if (req.file) {
+      toUpdate = { fullName, email, avatar: req.file.secure_url };
+    }
+
+    User.findByIdAndUpdate(
+      req.user._id, // the logged-in user's ID from Passport's "req.user"
+      { $set: toUpdate },
+      { runValidators: true }
+    )
+      .then(userDoc => {
+        req.flash("success", "Settings saved! 😉");
+        res.redirect("/");
+      })
+      .catch(err => next(err));
+  }
+);
 
 module.exports = router;
